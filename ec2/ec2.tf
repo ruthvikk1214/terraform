32e8ebe759b0289bf9475b5f334b94b78a98795c
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-04-04 15:23:20 +0530

    added terraform files

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+ ## vpc_id      = aws_vpc.main.id
 
   tags = {
     Name = "allow_tls"
