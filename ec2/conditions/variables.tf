a6aab6689397c3bc546a5892337235c4aaada204
Ruthvekh <ruthvikk1214@gmail.com>
2026-04-05 03:51:37 +0530

    added terraform files

---
+++ ec2/conditions/variables.tf
@@ -0,0 +1,26 @@
+variable "ami_id" {
+  default     = "ami-0220d79f3f480ecf5"
+  description = "RHEL9 image ID"
+  type        = string
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+  type    = string
+}
+
+
+variable "instance_tags" {
+    type = map(string)
+    default = {
+        Name = "variable-demo"
+        Project = "roboshop"
+        Terraform = "true"
+        environment = "dev"
+    }
+}
+
+variable "sg_name" {
+    type = string
+    default = "daws -allow-all"
+}
