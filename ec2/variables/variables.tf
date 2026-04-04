65a7a9310dbad57d705a8cb77a104903c81a877f
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-04-04 11:12:26 +0000

    added terraform files

--- ec2/variables/variables.tf
+++ ec2/variables/variables.tf
@@ -1,4 +1,10 @@
 variable "ami_id" {
-  default = "ami-0220d79f3f480ecf5"
+  default     = "ami-0220d79f3f480ecf5"
   description = "RHEL9 image ID"
+  type        = string
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+  type    = string
 }
