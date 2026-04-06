daa4609dd2f8204f7c07e14a8973bbd58f88eb7f
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-04-06 23:00:46 +0530

    added terraform files

--- ec2/count/ec2.tf
+++ ec2/count/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  count         = 10
+  count         = 11
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
