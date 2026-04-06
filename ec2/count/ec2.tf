a440d46185a144ec52b11ca8fd972491cbdb04db
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-04-06 22:49:58 +0530

    added terraform files

---
+++ ec2/count/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  count = 10
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  tags = {
+    Name        = "terraform"
+    Project     = "roboshop"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  ## vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
