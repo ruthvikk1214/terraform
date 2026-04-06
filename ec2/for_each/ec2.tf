eb37c06ab543a4b282231cede7d17fd466d9236f
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-04-07 02:09:27 +0530

    added terraform files

--- ec2/for_each/ec2.tf
+++ ec2/for_each/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "roboshop" {
 for_each = var.instances
+ ami           = "ami-0220d79f3f480ecf5"
   instance_type = "each.value"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
