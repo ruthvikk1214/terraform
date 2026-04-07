491d7f8fd5c24e9e099989d15433c7b78798c6c3
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-04-08 03:18:37 +0530

    added terraform files

---
+++ ec2/provisioners/ec2.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0eec592803ede7730"]
+  tags = {
+    Name        = "terraform-remote-state"
+    Project     = "roboshop"
+  }
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip}' > inventory.txt"
+  }
+}
+
+
+# resource "aws_security_group" "allow_tls" {
+#   name        = "allow_tls"
+#   description = "Allow TLS inbound traffic and all outbound traffic"
+#  ## vpc_id      = aws_vpc.main.id
+#
+#   tags = {
+#     Name = "allow_tls"
+#   }
+#
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+#
+#   ingress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+# }
