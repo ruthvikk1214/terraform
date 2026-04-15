2f632a74858ce5c27dda86bf8a5f25a06836f80b
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-04-15 22:17:40 +0000

    added terraform files

--- ec2/ec2instance-module/ec2.tf
+++ ec2/ec2instance-module/ec2.tf
@@ -1,35 +1,35 @@
 resource "aws_instance" "example" {
-  # ami           = "ami-0220d79f3f480ecf5"
-  # instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0eec592803ede7730"]
+   ami           = var.ami_id
+   instance_type = var.instance_type
+  vpc_security_group_ids = var.sg_ids
   tags = {
     Name        = "terraform-remote-state"
     Project     = "roboshop"
   }
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  tags = {
-    Name = "allow_tls"
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
+# resource "aws_security_group" "allow_tls" {
+#   name        = "allow_tls"
+#   description = "Allow TLS inbound traffic and all outbound traffic"
+#   vpc_id      = aws_vpc.main.id
+#
+#   tags = {
+#     Name = "allow_tls"
+#   }
+#
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+#
+#   ingress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+# }
--- ec2/ec2instance-module/variables.tf
+++ ec2/ec2instance-module/variables.tf
@@ -5,3 +5,14 @@ variable "instance_type" {
   type = string
   default = "t3.micro"
 }
+variable "sg_ids" {
+  type = list(string)
+  
+}
+variable "project" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
