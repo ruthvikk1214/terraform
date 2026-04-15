a6157cc124ff7d186e13fcac29191a107a31a2d2
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-04-15 22:30:18 +0000

    added terraform files

--- ec2/module-ec2/ec2.tf
+++ ec2/module-ec2/ec2.tf
@@ -1,4 +1,7 @@
 module "ec2" {
-    source = "./ec2"
-    
+    source = "../ec2-instance-module"
+    sg_ids = [sg-0eec592803ede7730]
+    ami_id = "ami-0220d79f3f480ecf5"
+    project = "roboshop"
+    environment = "dev"
 }
