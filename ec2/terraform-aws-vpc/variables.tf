1d149d688d3c0cd4a93bbd36f9f8bdc9b6c7ece9
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-04-16 22:59:21 +0000

    added terraform files

---
+++ ec2/terraform-aws-vpc/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default = "10.0.0.0/16"
+}
+variable "project" {
+  type = string
+}   
+variable "environment" {
+  type = string
+}
