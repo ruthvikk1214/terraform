8818349292eddc9bf4300543834fca341fb7cc4a
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-04-16 23:06:50 +0000

    added terraform files

--- ec2/vpc-module/provider.tf
+++ ec2/vpc-module/provider.tf
@@ -5,14 +5,6 @@ terraform {
       version = "6.39.0"
     }
   }
-   backend "s3" {
-    bucket         = "rk1214-remote-state"
-    key            = "remote-state.tfstate"
-    region         = "us-east-1"
-    ##dynamodb_table = "terraform-lock-table" # Optional: for locking
-    encrypt        = true                   # Recommended
-    use_lockfile = true
-  }
 }
 
 provider "aws" {
