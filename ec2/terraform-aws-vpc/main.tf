ea1748d2809ce7439c44c5bcf9e8bba46b302b55
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-04-17 22:11:49 +0000

    added terraform files

--- ec2/terraform-aws-vpc/main.tf
+++ ec2/terraform-aws-vpc/main.tf
@@ -1,19 +1,19 @@
-# resource "aws_vpc" "main" {
-#   cidr_block           = var.vpc_cidr
-#   instance_tenancy     = "default"
-#   enable_dns_hostnames = true
-#   tags = merge(local.common_tags, {
-#     Name = "${var.project}-${var.environment}-vpc"
-#   })
-# }
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-vpc"
+  })
+}
 
-# # Internet gateway attached to the main VPC
-# resource "aws_internet_gateway" "main" {
-#   vpc_id = aws_vpc.main.id
-#   tags = {
-#     Name = "main"
-#   }
-# }
+# Internet gateway attached to the main VPC
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main"
+  }
+}
 
 # Subnet within the main VPC
 resource "aws_subnet" "public" {
