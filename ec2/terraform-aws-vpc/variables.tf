8b3ab9ae9f4cfbc1e994e5cd5fff9bd686166b81
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-04-26 13:27:24 +0000

    added terraform files

--- ec2/terraform-aws-vpc/variables.tf
+++ ec2/terraform-aws-vpc/variables.tf
@@ -15,15 +15,15 @@ variable "igwy_tags" {
 }
 variable "public_subnet_cidrs" {
   type = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.1.0/24"]
 }
 variable "private_subnet_cidrs" {
   type = list(string)
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
+  default = ["10.0.2.0/24"]
 }
 variable "database_subnet_cidrs" {
   type = list(string)
-  default = ["10.0.5.0/24", "10.0.6.0/24"]
+  default = ["10.0.3.0/24"]
 }
 variable "availability_zones" {
   type = list(string)
