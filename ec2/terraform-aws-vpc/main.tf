a9e245adf26eecb1e5aea8126355508b5175f028
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-04-26 13:22:44 +0000

    added terraform files

--- ec2/terraform-aws-vpc/main.tf
+++ ec2/terraform-aws-vpc/main.tf
@@ -48,7 +48,7 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.main
+    #gateway_id = aws_internet_gateway.main
   }
 
   /*route {
