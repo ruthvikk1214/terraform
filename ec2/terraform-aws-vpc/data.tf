8639f9b6e40c33fb41091c2368bfd437fe7f36f7
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-04-27 06:06:29 +0000

    added terraform files

--- ec2/terraform-aws-vpc/data.tf
+++ ec2/terraform-aws-vpc/data.tf
@@ -4,11 +4,11 @@ data "aws_availability_zones" "available" {
 data "aws_vpc" "default_vpc_id" {
   default = true
 }
-data "aws_vpc" "default_vpc_id" {
+data "aws_vpc" "default_vpc" {
   # Filter by ID or another attribute
   id = aws_vpc.main.id
 }
 
 output "main_route_table_id" {
-  value = data.aws_vpc.default_vpc_id.main_route_table_id
+  value = data.aws_vpc.default_vpc.main_route_table_id
 }
