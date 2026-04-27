a78d7d56db6b31740145831063d537c8cc2e37b7
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-04-27 08:13:55 +0000

    added terraform files

--- ec2/roboshop-dev-infra/00-vpc/provider.tf
+++ ec2/roboshop-dev-infra/00-vpc/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "remote-state-roboshop-dev"
+    bucket         = "remote-state-roboshop-dev-rk1214"
     key            = "roboshop-dev-vpc"
     region         = "us-east-1"
     ##dynamodb_table = "terraform-lock-table" # Optional: for locking
