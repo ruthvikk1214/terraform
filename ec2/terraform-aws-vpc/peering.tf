c1669b43f0911ddbaaa7acc755ab26a365ef634d
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-06 15:39:05 +0530

    added terraform files

--- ec2/terraform-aws-vpc/peering.tf
+++ ec2/terraform-aws-vpc/peering.tf
@@ -39,6 +39,6 @@ resource "aws_route" "default_peering" {
 resource "aws_route" "database_peering" {
   count = var.is_peering_needed ? 1 : 0
   route_table_id            = aws_route_table.database.id
-  destination_cidr_block    = var.vpc_cidr
+  destination_cidr_block    = data.aws_vpc.default_vpc_id.cidr_block
   vpc_peering_connection_id  = aws_vpc_peering_connection.peering[count.index].id
 }
