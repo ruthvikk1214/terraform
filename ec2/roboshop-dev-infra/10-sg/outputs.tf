0ad0e62d9d7c4b124186537ca477194cb0c81b74
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-05-09 20:08:23 +0000

    added terraform files

--- ec2/roboshop-dev-infra/10-sg/outputs.tf
+++ ec2/roboshop-dev-infra/10-sg/outputs.tf
@@ -1,3 +1,10 @@
 output "sg_id" {
   value = aws_security_group.main.sg_id
 }
+ output "sg_id" {
+    description = "Security group ID from the SG module"
+    value       = module.sg.sg_id
+  }
+ output "mongodb_sg_id" {
+    value = module.sg.sg_id   # this will be the actual SG ID
+  }
