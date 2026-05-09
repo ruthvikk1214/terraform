2745dc607317e01efa99189f536f034fa41771dc
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-05-09 20:08:47 +0000

    added terraform files

--- ec2/terraform-aws-sg/outputs.tf
+++ ec2/terraform-aws-sg/outputs.tf
@@ -1,3 +1,7 @@
 output "sg_id" {
   value = aws_security_group.main.id
 }
+ output "sg_id" {
+    description = "The ID of the security group created by this module"
+    value       = aws_security_group.main.id
+  }
