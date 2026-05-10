22b91d91a389222c51c7dc52b04e2b3bc5c435e0
Ruthvekh <ruthvikk1214@gmail.com>
2026-05-10 15:04:08 +0530

    added terraform files

--- ec2/terraform-aws-vpc/outputs.tf
+++ ec2/terraform-aws-vpc/outputs.tf
@@ -4,3 +4,13 @@ output "vpc_id" {
 output "az_info" {
  value = data.aws_availability_zones.available.names 
 }
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+  
+}
