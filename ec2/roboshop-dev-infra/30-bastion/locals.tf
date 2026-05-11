8f61e10c2a660cdf52c561d9ccfa275144965319
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-12 03:21:04 +0530

    added terraform files

---
+++ ec2/roboshop-dev-infra/30-bastion/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  ami_id   =    data.aws_ami.joindevops.id
+  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+  bastion_sg_id =   data.aws_ssm_parameter.bastion_sg_id
+  common_tags  =    {
+    project =   var.project
+    environment =   var.environment
+    terraform   =   true
+  }
+}
