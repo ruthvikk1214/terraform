4f801589b31477ea0d8b1e870ce526f1d73afb47
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-12 03:23:39 +0530

    added terraform files

--- ec2/roboshop-dev-infra/30-bastion/main.tf
+++ ec2/roboshop-dev-infra/30-bastion/main.tf
@@ -36,10 +36,10 @@
   }
 }
 resource "aws_iam_role_policy_attachment" "bastion" {
-  role       = aws_iam_role.role.bastion
+  role       = aws_iam_role.bastion.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 resource "aws_iam_instance_profile" "bastion" {
   name = "${var.project}-${var.environment}-bastion"
-  role = aws_iam_role.role.bastion
+  role = aws_iam_role.bastion.name
 }
