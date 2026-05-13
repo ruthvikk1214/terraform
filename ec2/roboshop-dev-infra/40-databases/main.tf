1fe75eaf1482aa71a49c9cdc530f9ef69cdc10b3
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-05-13 07:50:47 +0000

    adding db

--- ec2/roboshop-dev-infra/40-databases/main.tf
+++ ec2/roboshop-dev-infra/40-databases/main.tf
@@ -8,5 +8,44 @@ resource "aws_instance" "mongodb" {
             Name = "${var.project}-${var.environment}-mongodb"
         },
         local.common_tags
+ )
+    }
+    
+resource "aws_instance" "redis" {
+    ami           =   local.ami_id
+    instance_type = "t3.micro"
+    subnet_id = local.database_subnet_ids
+    vpc_security_group_ids = [local.redis_sg_id]
+    tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-redis"
+        },
+        local.common_tags
+    )
+}
+
+resource "aws_instance" "rabbitmq" {
+    ami           =   local.ami_id
+    instance_type = "t3.micro"
+    subnet_id = local.database_subnet_ids
+    vpc_security_group_ids = [local.rabbitmq_sg_id]
+    tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-rabbitmq"
+        },
+        local.common_tags
+    )
+    }
+
+resource "aws_instance" "mysql" {
+    ami           =   local.ami_id
+    instance_type = "t3.micro"
+    subnet_id = local.database_subnet_ids
+    vpc_security_group_ids = [local.mysql_sg_id]
+    tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-mysql"
+        },
+        local.common_tags
     )
     }
