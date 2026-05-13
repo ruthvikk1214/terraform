9ff19258bf8697a8d954a263845a866da78b381f
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-13 16:27:08 +0530

    added terraform files

--- ec2/roboshop-dev-infra/40-databases/main.tf
+++ ec2/roboshop-dev-infra/40-databases/main.tf
@@ -11,44 +11,44 @@ resource "aws_instance" "mongodb" {
  )
     }
     
-resource "aws_instance" "redis" {
-    ami           =   local.ami_id
-    instance_type = "t3.micro"
-    subnet_id = local.database_subnet_ids
-    vpc_security_group_ids = [local.redis_sg_id]
-    tags = merge(
-        {
-            Name = "${var.project}-${var.environment}-redis"
-        },
-        local.common_tags
-    )
-}
+# resource "aws_instance" "redis" {
+#     ami           =   local.ami_id
+#     instance_type = "t3.micro"
+#     subnet_id = local.database_subnet_ids
+#     vpc_security_group_ids = [local.redis_sg_id]
+#     tags = merge(
+#         {
+#             Name = "${var.project}-${var.environment}-redis"
+#         },
+#         local.common_tags
+#     )
+# }
 
-resource "aws_instance" "rabbitmq" {
-    ami           =   local.ami_id
-    instance_type = "t3.micro"
-    subnet_id = local.database_subnet_ids
-    vpc_security_group_ids = [local.rabbitmq_sg_id]
-    tags = merge(
-        {
-            Name = "${var.project}-${var.environment}-rabbitmq"
-        },
-        local.common_tags
-    )
-    }
+# resource "aws_instance" "rabbitmq" {
+#     ami           =   local.ami_id
+#     instance_type = "t3.micro"
+#     subnet_id = local.database_subnet_ids
+#     vpc_security_group_ids = [local.rabbitmq_sg_id]
+#     tags = merge(
+#         {
+#             Name = "${var.project}-${var.environment}-rabbitmq"
+#         },
+#         local.common_tags
+#     )
+#     }
 
-resource "aws_instance" "mysql" {
-    ami           =   local.ami_id
-    instance_type = "t3.micro"
-    subnet_id = local.database_subnet_ids
-    vpc_security_group_ids = [local.mysql_sg_id]
-    tags = merge(
-        {
-            Name = "${var.project}-${var.environment}-mysql"
-        },
-        local.common_tags
-    )
-    }
+# resource "aws_instance" "mysql" {
+#     ami           =   local.ami_id
+#     instance_type = "t3.micro"
+#     subnet_id = local.database_subnet_ids
+#     vpc_security_group_ids = [local.mysql_sg_id]
+#     tags = merge(
+#         {
+#             Name = "${var.project}-${var.environment}-mysql"
+#         },
+#         local.common_tags
+#     )
+#     }
 
 resource "terraform_data" "bootstrap" {
   triggers_replace = [
@@ -62,8 +62,15 @@ connection {
       password = "DevOps321"
   }
 
+provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
   provisioner "remote-exec" {
-    
-      
+    inline = [ 
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh"
+    ]
   }
 }
