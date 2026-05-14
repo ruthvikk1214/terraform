e60639c5220413625dc3db5d2cdd3befe410a206
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-15 00:50:27 +0530

    added terraform files

--- ec2/roboshop-dev-infra/40-databases/bootstrap.sh
+++ ec2/roboshop-dev-infra/40-databases/bootstrap.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+component=$1
 dnf install ansible -y 
+cd /home/ec2-user
+git clone https://github.com/ruthvikk1214/roboshop-ansible-roles-tf.git
+cd roboshop-ansible-roles-tf
+cd ansible-roboshop-roles-tf/
+ansible-playbook -e component=$component roboshop.yaml
