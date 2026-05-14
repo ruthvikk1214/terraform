15e68d83f5d45baa69db664c19545b0b1fb5fd05
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-14 19:46:09 +0530

    added terraform files

--- ec2/roboshop-dev-infra/20-sg-rules/main.tf
+++ ec2/roboshop-dev-infra/20-sg-rules/main.tf
@@ -3,8 +3,8 @@ resource "aws_security_group_rule" "bastion_internet" { #bastion accepting conne
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-#   cidr_blocks       = [0.0.0.0/0]
-  cidr_blocks       = [local.my_ip]
+  cidr_blocks = ["0.0.0.0/0"]
+  
   security_group_id = local.bastion_sg_id
 }
 
