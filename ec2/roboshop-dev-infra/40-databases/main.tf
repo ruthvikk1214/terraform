dbc3085ce75f89c3e1f31a0d5e02ecab63532b09
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-15 01:34:52 +0530

    added terraform files

--- ec2/roboshop-dev-infra/40-databases/main.tf
+++ ec2/roboshop-dev-infra/40-databases/main.tf
@@ -11,6 +11,44 @@ resource "aws_instance" "mongodb" {
  )
 }
 
+resource "terraform_data" "bootstrap-mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id,
+  ]
+
+connection {
+      type        = "ssh"
+      host        = aws_instance.mongodb.private_ip
+      user        = "ec2-user"
+      password = "DevOps321"
+  }
+
+provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo /tmp/bootstrap.sh mongodb"
+    ]
+  }
+}
+    
+resource "aws_instance" "redis" {
+    ami           =   local.ami_id
+    instance_type = "t3.micro"
+    subnet_id = local.database_subnet_ids
+    vpc_security_group_ids = [local.redis_sg_id]
+    tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-redis"
+        },
+        local.common_tags
+    )
+}
+
 resource "terraform_data" "bootstrap-redis" {
   triggers_replace = [
     aws_instance.redis.id,
@@ -35,43 +73,7 @@ provisioner "file" {
     ]
   }
 }
-    
-resource "aws_instance" "redis" {
-    ami           =   local.ami_id
-    instance_type = "t3.micro"
-    subnet_id = local.database_subnet_ids
-    vpc_security_group_ids = [local.redis_sg_id]
-    tags = merge(
-        {
-            Name = "${var.project}-${var.environment}-redis"
-        },
-        local.common_tags
-    )
-}
-resource "terraform_data" "bootstrap" {
-  triggers_replace = [
-    aws_instance.mongodb.id,
-  ]
 
-connection {
-      type        = "ssh"
-      host        = aws_instance.mongodb.private_ip
-      user        = "ec2-user"
-      password = "DevOps321"
-  }
-
-provisioner "file" {
-    source      = "bootstrap.sh"
-    destination = "/tmp/bootstrap.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [ 
-      "chmod +x /tmp/bootstrap.sh",
-      "sudo /tmp/bootstrap.sh mongodb"
-    ]
-  }
-}
 
 # resource "aws_instance" "rabbitmq" {
 #     ami           =   local.ami_id
