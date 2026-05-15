755191a3ea051fe5161234a08c5f0a356d3fcb55
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-16 02:55:31 +0530

    added terraform files

--- ec2/roboshop-dev-infra/40-databases/bootstrap.sh
+++ ec2/roboshop-dev-infra/40-databases/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 component=$1
 dnf install ansible -y 
+ansible-galaxy collection install community.mysql
 cd /home/ec2-user
 git clone https://github.com/ruthvikk1214/roboshop-ansible-roles-tf.git
 cd roboshop-ansible-roles-tf
