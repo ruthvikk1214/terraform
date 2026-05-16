4f99c2a4d7e110a3abc197177e51dd7e6a3bec67
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-16 14:19:29 +0530

    added roboshop files

--- ec2/roboshop-dev-infra/40-databases/bootstrap.sh
+++ ec2/roboshop-dev-infra/40-databases/bootstrap.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 component=$1
-dnf install ansible -y 
-ansible-galaxy collection install community.mysql
+
+dnf install ansible -y
+ansible-galaxy collection install amazon.aws
+
 cd /home/ec2-user
+
+rm -rf roboshop-ansible-roles-tf
 git clone https://github.com/ruthvikk1214/roboshop-ansible-roles-tf.git
+
 cd roboshop-ansible-roles-tf
 cd ansible-roboshop-roles-tf/
+
 ansible-playbook \
   -e "component=$component env=dev" \
-  -e "MYSQL_ROOT_PASSWORD=$mysql_password" \
   roboshop.yaml
