b099c2d5f2cb72ae272a413b9d7b6565bb69b265
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-17 18:08:27 +0530

    added roboshop files

--- ec2/roboshop-dev-infra/20-sg-rules/data.tf
+++ ec2/roboshop-dev-infra/20-sg-rules/data.tf
@@ -40,3 +40,6 @@ data "aws_ssm_parameter" "payment_sg_id" {
 data "aws_ssm_parameter" "frontend_sg_id" {
   name = "/${var.project}/${var.environment}/frontend_sg_id"
 }
+data "aws_ssm_parameter" "backend_alb_sg_id" {
+  name = "/${var.project}/${var.environment}/backend_alb_sg_id"
+}
--- ec2/roboshop-dev-infra/20-sg-rules/main.tf
+++ ec2/roboshop-dev-infra/20-sg-rules/main.tf
@@ -164,3 +164,12 @@ resource "aws_security_group_rule" "frontend_bastion" {
   security_group_id        = local.frontend_sg_id
 }
 
+resource "aws_security_group_rule" "backend-alb_bastion" {
+  type      = "ingress"
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id        = local.backend_alb_sg_id
+}
