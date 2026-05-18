bdf9a813afa95aa7481808e40da958ff86dba249
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-19 02:03:06 +0530

    added backend-alb files

--- ec2/roboshop-dev-infra/50-backcend-alb/locals.tf
+++ ec2/roboshop-dev-infra/50-backcend-alb/locals.tf
@@ -1,3 +1,9 @@
 locals {
-  backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id
+  backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+  private_subnet_ids =  split(",", data.aws_ssm_parameter.private_subnets_ids.value)
+  common_tags  =    {
+    project =   var.project
+    environment =   var.environment
+    terraform   =   true
+  }
 }
--- ec2/roboshop-dev-infra/50-backcend-alb/main.tf
+++ ec2/roboshop-dev-infra/50-backcend-alb/main.tf
@@ -1,19 +1,42 @@
 resource "aws_lb" "backend-alb" {
   name               = "${var.project}-${var.environment}"
-  internal           = false
+  internal           = true
   load_balancer_type = "application"
   security_groups    = [local.backend_alb_sg_id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  subnets            = local.private_subnet_ids
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "${var.project}-${var.environment}-backend-alb"
-    enabled = true
+  tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-backend-alb"
+        },
+        local.common_tags
+ )
+}
+
+resource "aws_lb_listener" "HTTP" {
+  load_balancer_arn = aws_lb.backend-alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1> Backend ALB is working fine</h1>"
+      status_code = "200"
+    }
   }
+}
+resource "aws_route53_record" "backend_alias" {
+  zone_id = var.zone_id
+  name    = "*.backend-alb-${var.project}-${var.project}-${var.domain_name}" # The DNS name you want to use
+  type    = "A"               # Use "A" for IPv4 or "AAAA" for IPv6
 
-  tags = {
-    Name = "${var.project}-${var.environment}-backend-alb"
+  alias {
+    # Reference the ALB's DNS name and Hosted Zone ID
+    name                   = aws_lb.backend.dns_name
+    zone_id                = aws_lb.backend.zone_id
+    evaluate_target_health = true # Recommended to let Route 53 check ALB health
   }
 }
