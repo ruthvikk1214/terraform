d304fd468f173aab1b6feedb172132cb6ce1970e
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-20 02:55:37 +0530

    added catalogue related files

--- ec2/roboshop-dev-infra/60-catalogue/locals.tf
+++ ec2/roboshop-dev-infra/60-catalogue/locals.tf
@@ -1,7 +1,7 @@
 locals {
   catalogue_sg_id    = data.aws_ssm_parameter.catalogue_sg_id.value
   ami_id             = data.aws_ami.joindevops.id
-  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnets_ids.value)[0]
+  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
   common_tags = {
     project     = var.project
     environment = var.environment
