31913fc6a8adad3aa8a77c746cadcd3dc80f8b84
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-20 02:16:00 +0530

    added database related files

--- ec2/roboshop-dev-infra/40-databases/data.tf
+++ ec2/roboshop-dev-infra/40-databases/data.tf
@@ -14,35 +14,11 @@ data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }
 
-/*
-data "aws_ssm_parameter" "user_sg_id" {
-  name = "/${var.project}/${var.environment}/user_sg_id"
-}
-
-data "aws_ssm_parameter" "frontend_sg_id" {
-  name = "/${var.project}/${var.environment}/frontend_sg_id"
-}
-data "aws_ssm_parameter" "payment_sg_id" {
-  name = "/${var.project}/${var.environment}/payment_sg_id"
-}
-data "aws_ssm_parameter" "cart_sg_id" {
-  name = "/${var.project}/${var.environment}/cart_sg_id"
-}
-
-data "aws_ssm_parameter" "catalogue_sg_id" {
-  name = "/${var.project}/${var.environment}/catalogue_sg_id"
-}
-*/
 
  data "aws_ssm_parameter" "redis_sg_id" {
    name = "/${var.project}/${var.environment}/redis_sg_id"
  }
 
-/*
-data "aws_ssm_parameter" "shipping_sg_id" {
-   name = "/${var.project}/${var.environment}/shipping_sg_id"
- }
-*/
 
 data "aws_ssm_parameter" "rabbitmq_sg_id" {
   name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
@@ -52,12 +28,6 @@ data "aws_ssm_parameter" "mysql_sg_id" {
   name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
 
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project}/${var.environment}/private_subnet_ids"
-}
-data "aws_ssm_parameter" "public_subnet_ids" {
-  name = "/${var.project}/${var.environment}/public_subnet_ids"
-}
 data "aws_ssm_parameter" "database_subnet_ids" {
   name = "/${var.project}/${var.environment}/database_subnet_ids"
 }
--- ec2/roboshop-dev-infra/40-databases/locals.tf
+++ ec2/roboshop-dev-infra/40-databases/locals.tf
@@ -4,16 +4,8 @@ locals {
   ami_id   =    data.aws_ami.joindevops.id
    rabbitmq_sg_id =   data.aws_ssm_parameter.rabbitmq_sg_id.value
    redis_sg_id =   data.aws_ssm_parameter.redis_sg_id.value
-#    catalogue_sg_id  = data.aws_ssm_parameter.catalogue_sg_id.value
    mysql_sg_id =   data.aws_ssm_parameter.mysql_sg_id.value
-#    user_sg_id = data.aws_ssm_parameter.user_sg_id.value
-#    cart_sg_id = data.aws_ssm_parameter.cart_sg_id.value
-#    frontend_sg_id =  data.aws_ssm_parameter.frontend_sg_id.value
-#    payment_sg_id  =  data.aws_ssm_parameter.payment_sg_id.value
-#    shipping_sg_id = data.aws_ssm_parameter.shipping_sg_id.value
   database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
-  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
 mysql_role_name = join("-", [
             for name in ["${var.project}","${var.environment}", "mysql"] : title(name)
         ])
