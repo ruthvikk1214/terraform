4b38c9a549ec61bb0e0376e2fc01074dca6b425d
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-21 16:19:18 +0530

    added template code

--- ec2/roboshop-dev-infra/60-catalogue/locals.tf
+++ ec2/roboshop-dev-infra/60-catalogue/locals.tf
@@ -1,6 +1,7 @@
 locals {
   catalogue_sg_id    = data.aws_ssm_parameter.catalogue_sg_id.value
   ami_id             = data.aws_ami.joindevops.id
+  vpc_id             = data.aws_ssm_parameter.vpc_id.value
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
   common_tags = {
     project     = var.project
@@ -8,3 +9,4 @@ locals {
     terraform   = true
   }
 }
+
--- ec2/roboshop-dev-infra/60-catalogue/main.tf
+++ ec2/roboshop-dev-infra/60-catalogue/main.tf
@@ -53,4 +53,67 @@ resource "aws_ami_from_instance" "catalogue" {
     local.common_tags
   )
 }
+
+resource "aws_lb_target_group" "catalogue" {
+  name                 = "${var.project}-${var.environment}-catalogue"
+  port                 = 8080
+  protocol             = "HTTP"
+  vpc_id               = local.vpc_id
+  deregistration_delay = 60
+  health_check {
+    path                = "/health"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 10
+    matcher             = "200-299"
+  }
+}
+
+resource "aws_launch_template" "catalogue" {
+  name                                 = "${var.project}-${var.environment}-catalogue"
+  instance_initiated_shutdown_behavior = "terminate"
+  image_id                             = aws_ami_from_instance.catalogue.id
+  instance_type                        = "t3.micro"
+  vpc_security_group_ids               = [local.catalogue_sg_id]
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge(
+      {
+        Name = "${var.project}-${var.environment}-catalogue"
+      },
+      local.common_tags
+    )
+  }
+}
+
+resource "aws_autoscaling_group" "catalogue" {
+  name                = "${var.project}-${var.environment}-catalogue"
+  vpc_zone_identifier = local.private_subnet_ids
+  target_group_arns   = [aws_lb_target_group.catalogue.arn]
+  min_size            = 1
+  max_size            = 2
+  desired_capacity    = 1
   
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = "$Latest"
+  }
+
+  dynamic "tag" {
+    for_each = merge(
+      {
+        Name = "${var.project}-${var.environment}-catalogue"
+      },
+      local.common_tags
+    )
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+}
+
