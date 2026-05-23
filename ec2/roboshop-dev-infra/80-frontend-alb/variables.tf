a0bbce43c84819b17378164a86e38832fdbc05dc
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-23 22:16:22 +0530

    update 80-frontend-alb with dynamic ssl and routing config

--- ec2/roboshop-dev-infra/80-frontend-alb/variables.tf
+++ ec2/roboshop-dev-infra/80-frontend-alb/variables.tf
@@ -8,12 +8,11 @@ variable "environment" {
 
 variable "zone_id" {
   default = "Z031906510N5GWM6MW07L"
-} 
+}
 
 variable "domain_name" {
   default = "rk1214.in"
 }
-variable "mysql_root_password" {
-  default   = "DevOps321"
-  sensitive = true
+variable "acm_arn" {
+  default = "arn:aws:acm:us-east-1:114087031553:certificate/e18275d3-b223-44a4-8022-2ff08d72fa65"
 }
