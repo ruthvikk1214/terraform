b11d9318b875f79d9c2b20a9992ef9ce79fd8581
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-24 03:55:09 +0530

    updated

--- ec2/roboshop-dev-infra/90-components/main.tf
+++ ec2/roboshop-dev-infra/90-components/main.tf
@@ -1,5 +1,5 @@
 module "component" {
-  source        = "git::https://github.com/daws-88s/terraform-roboshop-component.git?ref=main"
+  source        = "git::https://github.com/ruthvikk1214/terraform-roboshop-component.git?ref=main"
   for_each      = var.component
   component     = each.key
   rule_priority = each.value.rule_priority
