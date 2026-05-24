9944a1334bde2be423328cf2131fd0b0c3ec5aa1
ruthvikk1214 <ruthvikk1214@gmail.com>
2026-05-24 20:59:39 +0530

    updated component

--- ec2/roboshop-dev-infra/90-components/variables.tf
+++ ec2/roboshop-dev-infra/90-components/variables.tf
@@ -1,22 +1,22 @@
 variable "component" {
   default = {
     catalogue = {
-      rule_priority = 10
+      rule_priority = 110
     },
     cart = {
-      rule_priority = 11
+      rule_priority = 111  # Moving away from the stuck priority 11 slot
     },
     shipping = {
-      rule_priority = 11
+      rule_priority = 114
     },
     payment = {
-      rule_priority = 12
+      rule_priority = 112
     },
     user = {
-      rule_priority = 13
+      rule_priority = 113
     },
     frontend = {
-      rule_priority = 5
+      rule_priority = 105
     }
   }
 }
